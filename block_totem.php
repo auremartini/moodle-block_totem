5df18b6c0614671de20f2e0fa8767f759f18d724
auremartini <aure.martini@gmail.com>
2020-09-17 20:21:35 +0200

    Basic block

---
+++ block_totem.php
@@ -0,0 +1,63 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @since     Moodle 3.6
+ * @package   block_totem
+ * @copyright 2020 Aureliano Martini
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class block_totem extends block_base {
+
+    /**
+     * Set the initial properties for the block
+     */
+    function init() {
+        $this->blockname = get_class($this);
+        $this->title = get_string('pluginname', $this->blockname);
+    }
+
+    /**
+     * All multiple instances of this block
+     * @return bool Returns false
+     */
+    function instance_allow_multiple() {
+        return false;
+    }
+
+    /**
+     * Gets the content for this block by grabbing it from $this->page
+     *
+     * @return object $this->content
+     */
+    function get_content() {
+        // First check if we have already generated, don't waste cycles
+        if ($this->content !== NULL) {
+            return $this->content;
+        }
+        
+        $this->content = new stdClass();
+        
+        $this->content->text = 'This is the text';
+        $this->content->footer = 'This is the footer';
+        
+
+
+        return $this->content;
+    }
+
+}
