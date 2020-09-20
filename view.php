46051a6ae97406093aa612fb5e0a987c132a3768
auremartini <aure.martini@gmail.com>
2020-09-20 13:40:16 +0200

    Basic event list

--- view.php
+++ view.php
@@ -35,8 +35,7 @@ $blockname = 'totem';
 $blockid = required_param('blockid', PARAM_INT);
 $blockinstance = $DB->get_records('block_instances', array('id' => $blockid));
 $block = block_instance($blockname, $blockinstance[$blockid]);
-$date = optional_param('date', 0, PARAM_INT);
-
+$date = optional_param('date', now(), PARAM_INT);
 
 // SET PAGE ELEMENTS (HEADER)
 $PAGE->set_url(new moodle_url('/blocks/totem/view.php'));
@@ -51,35 +50,50 @@ $node->make_active();
 // PRINT CONTENT TO PAGE
 echo $OUTPUT->header();
 
-echo '<table width="100%"><tr><td style="vertical-align: top; width:15rem;">';
-echo $OUTPUT->box_start();
-echo 'DATE PICKER';
-echo $OUTPUT->box_end();
-
-echo '<p><form method="post" action="'.$url.'">
-      <button type="submit" class="btn btn-secondary" title="">'.get_string('slideshow', 'block_totem').'</button>
-      </form></p>';
+echo html_writer::start_tag('div');
 
 $url = new moodle_url('/blocks/totem/event.php', array('blockid' => $blockid, 'cohortsourceid' => $block->config->cohortsourceid));
 echo '<p><form method="post" action="'.$url.'">
       <button type="submit" class="btn btn-secondary" title="">'.get_string('addtotemelement', 'block_totem').'</button>
       </form></p>';
+echo html_writer::end_tag('div');
 
-echo '</td><td style="width:1.25rem;">&nbsp;</td><td style="vertical-align: top;">';
-echo '<div class="totem-box">';
 
-if ($rs = $DB->get_records('block_totem_event', array('blockid' => $blockid))) {
-    echo html_writer::start_tag('ul');
+echo html_writer::start_tag('div', array('class' => 'totem-table'));
+echo html_writer::start_tag('table', array('class' => 'generaltable', 'width' => '100%'));
+//echo html_writer::start_tag('thead');
+//echo html_writer::start_tag('tr');
+//echo html_writer::start_tag('th', array('class' => 'header c0')).''.html_writer::end_tag('th');
+//echo html_writer::start_tag('th', array('class' => 'header c1')).'Teacher'.html_writer::end_tag('th');
+//echo html_writer::start_tag('th', array('class' => 'header c2')).'Message'.html_writer::end_tag('th');
+//echo html_writer::end_tag('tr');
+//echo html_writer::end_tag('thead');
+$sql = "SELECT te.id, te.eventtype, u.idnumber, te.subject, te.section, te.time, te.displaytext
+        FROM mdl_block_totem_event te
+        LEFT JOIN mdl_user u ON te.userid = u.id
+        WHERE te.blockid = :blockid
+        ORDER BY te.date, te.time, u.idnumber";
+if ($rs = $DB->get_records_sql($sql, array('blockid' => $blockid))) {
+    echo html_writer::start_tag('tbody');
     foreach ($rs as $record) {
-        $pageurl = new moodle_url('/blocks/totem/event.php', array('id' => $record->id, 'blockid' => $blockid, 'cohortsourceid' => $block->config->cohortsourceid));
-        echo html_writer::start_tag('li');
-        echo html_writer::link($pageurl, '[['.$record->id.']]');
-        echo html_writer::end_tag('li');
+        echo html_writer::start_tag('tr');
+        echo html_writer::start_tag('td', array('class' => 'cell c0', 'width' => '50rem')).$record->eventtype.html_writer::end_tag('td');
+        echo html_writer::start_tag('td', array('class' => 'cell c1')).$record->idnumber.html_writer::end_tag('td');
+        echo html_writer::start_tag('td', array('class' => 'cell c2', 'width' => '100rem')).$record->subject.html_writer::end_tag('td');
+        echo html_writer::start_tag('td', array('class' => 'cell c3', 'width' => '100rem')).$record->section.html_writer::end_tag('td');
+        echo html_writer::start_tag('td', array('class' => 'cell c4', 'width' => '100rem')).$record->time.html_writer::end_tag('td');
+        echo html_writer::start_tag('td', array('class' => 'cell c5')).$record->displaytext.html_writer::end_tag('td');
+        if (TRUE) {
+            $url = new moodle_url('/blocks/totem/event.php', array('id' => $record->id, 'blockid' => $blockid, 'cohortsourceid' => $block->config->cohortsourceid));
+            echo html_writer::start_tag('td', array('class' => 'cell c5', 'width' => '50rem'));
+            echo html_writer::link($url, '[[E]]');
+            echo html_writer::end_tag('td');
+        }
+        echo html_writer::end_tag('tr');
     }
-    echo html_writer::end_tag('ul');
+    echo html_writer::end_tag('tbody');
 }
-
-echo '</div>';
-echo '</td></tr></table>';
+echo html_writer::end_tag('table');
+echo html_writer::end_tag('div');
 
 echo $OUTPUT->footer();
