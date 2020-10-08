dc243a4ed94c23ac0f7d1d06b4831f6c0b91f5ff
auremartini <aure.martini@gmail.com>
2020-10-08 23:00:26 +0200

    Starting moving js to AMD

---
+++ amd/src/event_edit_form.js
@@ -0,0 +1,20 @@
+define(['jquery'], function($) {
+    return {
+        init: function() {
+			require(['jquery', 'core/ajax', 'core/notification'], function($, ajax, notification) {
+				var promises = ajax.call([
+					{ methodname: 'get_userlist', args: { cohortid: 1 } }
+				]);
+				promises[0].done(function(response) {
+					document.getElementById('id_userid').innerHTML = "";
+					response.forEach(function(item, index) {
+						var option = document.createElement("option");
+						option.value = item.id;
+						option.text = item.firstname + " " + item.lastname;
+						document.getElementById('id_userid').add(option);
+					});
+				}).fail(notification.exception);
+			});
+        }
+    };
+});
