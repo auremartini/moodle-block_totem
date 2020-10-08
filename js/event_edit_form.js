0af1549f0786a141d9bf2d43e84c9ba94a8967aa
auremartini <aure.martini@gmail.com>
2020-10-08 21:23:31 +0200

    Minor fix

--- js/event_edit_form.js
+++ js/event_edit_form.js
@@ -1,18 +1,14 @@
-var option = document.createElement("option");
-option.value = "My";
-option.text = "Aureliano Martini";
-
 require(['jquery', 'core/ajax', 'core/notification'], function($, ajax, notification) {
 	var promises = ajax.call([
 		{ methodname: 'get_userlist', args: { cohortid: 1 } }
 	]);
 	promises[0].done(function(response) {
-//		document.getElementById('id_userid').innerHTML = "";
+		document.getElementById('id_userid').innerHTML = "";
 		response.forEach(function(item, index) {
 			var option = document.createElement("option");
 			option.value = item.id;
 			option.text = item.firstname + " " + item.lastname;
-//			document.getElementById('id_userid').add(option);
+			document.getElementById('id_userid').add(option);
 		});
 	}).fail(notification.exception);
 });
