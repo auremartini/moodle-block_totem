302a94cfc1622ab497317e0242f75f933879bfb9
auremartini <aure.martini@gmail.com>
2020-10-10 21:44:01 +0200

    GRUNT & go

--- amd/src/event_edit_form.js
+++ amd/src/event_edit_form.js
@@ -3,26 +3,26 @@
  * @package   block_totem
  * @copyright 2020 Aureliano Martini
  * @licence   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @since	  3.0
+ * @since     3.0
  */
 
-define(['jquery'], function($) {
+define(['jquery'], function() {
     return {
         init: function() {
-			require(['jquery', 'core/ajax', 'core/notification'], function($, ajax, notification) {
-				var promises = ajax.call([
-					{ methodname: 'get_userlist', args: { cohortid: 1 } }
-				]);
-				promises[0].done(function(response) {
-					document.getElementById('id_userid').innerHTML = "";
-					response.forEach(function(item, index) {
-						var option = document.createElement("option");
-						option.value = item.id;
-						option.text = item.firstname + " " + item.lastname;
-						document.getElementById('id_userid').add(option);
-					});
-				}).fail(notification.exception);
-			});
+            require(['jquery', 'core/ajax', 'core/notification'], function($, ajax, notification) {
+                var promises = ajax.call([
+                    { methodname: 'get_userlist', args: { cohortid: 1 } }
+                ]);
+                promises[0].done(function(response) {
+                    document.getElementById('id_userid').innerHTML = "";
+                    response.forEach(function(item) {
+                        var option = document.createElement("option");
+                        option.value = item.id;
+                        option.text = item.firstname + " " + item.lastname;
+                        document.getElementById('id_userid').add(option);
+                    });
+                }).fail(notification.exception);
+             });
         }
     };
 });
--- amd/src/slideshow.js
+++ amd/src/slideshow.js
@@ -3,13 +3,13 @@
  * @package   blocktotem
  * @copyright 2020 Aureliano Martini
  * @licence   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @since	  3.0
+ * @since     3.0
  */
 
 define(['jquery', 'core/ajax', 'core/templates', 'core/notification'], function($, ajax, template, notification) {
-	return {
-		refresh: function() {
-			alert('AAA');
-		}
-	}
+    return {
+        refresh: function() {
+            alert('AAA');
+        }
+    };
 });
